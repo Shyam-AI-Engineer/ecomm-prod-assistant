9d6d6b307eddeef6b355e446eb50159e3956345d
Shyam-AI-Engineer <shyamgenaiengineer@gmail.com>
2026-02-09 13:30:03 +0530

    code evaluation added

--- pro_assistant/evaluation/ragas_eval.py
+++ pro_assistant/evaluation/ragas_eval.py
@@ -5,3 +5,24 @@ from ragas.llms import LangchainLLMWrapper
 from ragas.embeddings import LangchainEmbeddingsWrapper
 from ragas.metrics import LLMContextPrecisionWithoutReference, ResponseRelevancy
 import grpc.experimental.aio as grpc_aio
+grpc_aio.init_grpc_aio()
+model_loader=ModelLoader()
+
+def evaluate_context_precision(query, response, retrieved_context):
+    try:
+        sample = SingleTurnSample(
+            user_input=query,
+            response=response,
+            retrieved_contexts=retrieved_context,
+        )
+
+        async def main():
+            llm = model_loader.load_llm()
+            evaluator_llm = LangchainLLMWrapper(llm)
+            context_precision = LLMContextPrecisionWithoutReference(llm=evaluator_llm)
+            result = await context_precision.single_turn_ascore(sample)
+            return result
+
+        return asyncio.run(main())
+    except Exception as e:
+        return e
