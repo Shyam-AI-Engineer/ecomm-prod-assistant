2b60a7f72095d38ecce2d48d1a34aea1a4a6a3b1
Shyam-AI-Engineer <shyamgenaiengineer@gmail.com>
2026-02-11 15:50:21 +0530

    code updated for mcp

--- pro_assistant/mcp_servers/client.py
+++ pro_assistant/mcp_servers/client.py
@@ -11,3 +11,20 @@ async def main():
             "transport": "stdio",
         }
     })
+    
+    # Discover tools
+    tools = await client.get_tools()
+    print("Available tools:", [t.name for t in tools])
+
+    # Pick tools by name
+    retriever_tool = next(t for t in tools if t.name == "get_product_info")
+    web_tool = next(t for t in tools if t.name == "web_search")
+
+    # --- Step 1: Try retriever first ---
+    #query = "Samsung Galaxy S25 price"
+    # query = "iPhone 15"
+    query = "iPhone 17?"
+    retriever_result = await retriever_tool.ainvoke({"query": query})
+    print("\nRetriever Result:\n", retriever_result)
+    
+    
